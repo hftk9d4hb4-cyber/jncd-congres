826b51174d6f2d40b57829af011b71cbdebc7748
hftk9d4hb4-cyber <hftk9d4hb4@privaterelay.appleid.com>
2026-07-19 15:16:11 +0200

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  poweredByHeader: false,
-  async headers() {
-    return [{ source: "/:path*", headers: [{ key: "X-Robots-Tag", value: "noindex, nofollow, noarchive" }, { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" }, { key: "X-Content-Type-Options", value: "nosniff" }] }];
-  },
-};
-export default nextConfig;
